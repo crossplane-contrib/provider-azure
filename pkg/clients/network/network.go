21b106cc165e51f5aef62eb5fae757a23c5f5c8a
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-08-31 14:35:53 +0300

    virtualnetwork: migrate to using ProviderConfig
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

--- pkg/clients/network/network.go
+++ pkg/clients/network/network.go
@@ -17,50 +17,13 @@ limitations under the License.
 package network
 
 import (
-	"context"
-	"encoding/json"
 	"reflect"
 
 	networkmgmt "github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-06-01/network"
-	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-06-01/network/networkapi"
-	"github.com/Azure/go-autorest/autorest/azure/auth"
-	"github.com/pkg/errors"
 
 	"github.com/crossplane/provider-azure/apis/network/v1alpha3"
 	azure "github.com/crossplane/provider-azure/pkg/clients"
 )
-
-// A VirtualNetworksClient handles CRUD operations for Azure Virtual Networks.
-type VirtualNetworksClient networkapi.VirtualNetworksClientAPI
-
-// NewVirtualNetworksClient returns a new Azure Virtual Networks client. Credentials must be
-// passed as JSON encoded data.
-func NewVirtualNetworksClient(ctx context.Context, credentials []byte) (VirtualNetworksClient, error) {
-	c := azure.Credentials{}
-	if err := json.Unmarshal(credentials, &c); err != nil {
-		return nil, errors.Wrap(err, "cannot unmarshal Azure client secret data")
-	}
-
-	client := networkmgmt.NewVirtualNetworksClient(c.SubscriptionID)
-
-	cfg := auth.ClientCredentialsConfig{
-		ClientID:     c.ClientID,
-		ClientSecret: c.ClientSecret,
-		TenantID:     c.TenantID,
-		AADEndpoint:  c.ActiveDirectoryEndpointURL,
-		Resource:     c.ResourceManagerEndpointURL,
-	}
-	a, err := cfg.Authorizer()
-	if err != nil {
-		return nil, errors.Wrapf(err, "cannot create Azure authorizer from credentials config")
-	}
-	client.Authorizer = a
-	if err := client.AddToUserAgent(azure.UserAgent); err != nil {
-		return nil, errors.Wrap(err, "cannot add to Azure client user agent")
-	}
-
-	return client, nil
-}
 
 // NewVirtualNetworkParameters returns an Azure VirtualNetwork object from a virtual network spec
 func NewVirtualNetworkParameters(v *v1alpha3.VirtualNetwork) networkmgmt.VirtualNetwork {
