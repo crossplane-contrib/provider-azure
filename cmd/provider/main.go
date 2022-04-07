d77df144483a99a5aea92a95be0b04fd58758363
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2022-04-07 13:37:47 +0300

    DO NOT MERGE
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -12,6 +12,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+
 */
 
 package main
@@ -28,6 +29,7 @@ import (
 	"github.com/crossplane/crossplane-runtime/pkg/ratelimiter"
 
 	"github.com/crossplane/provider-azure/apis"
+
 	"github.com/crossplane/provider-azure/pkg/controller"
 )
 
